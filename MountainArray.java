07af41a061cde76ca550bb4989a1617b74b068d5
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-02-04 22:04:28 +0530

    M array

---
+++ MountainArray.java
@@ -0,0 +1,29 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package array;
+
+public class MountainArray {
+    public static void main(String[] args) {
+        int[] arr = new int[]{0, 3, 2, 1, 0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+
+    public static int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] > arr[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return start;
+    }
+}
