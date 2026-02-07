b1ae74d9abc108dc287817a28b4c967641b92a5d
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-02-07 12:08:15 +0530

    array

---
+++ strings/Main.java
@@ -0,0 +1,11 @@
+package strings;
+
+public class Main {
+    public static void main(String[] args) {
+//        String contact1 = "Abhi";
+//        String contact2 = "Abhi";
+        String a =  "abhi";
+        String b = new String("abhi");
+        System.out.println(a.equals(b));
+    }
+}
