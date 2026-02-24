47ef5bab1cc3bacb6058cbd86ba8feef5ef10c4d
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-02-24 18:52:25 +0530

    Modified Fibonacci

--- recursion/Fibo.java
+++ recursion/Fibo.java
@@ -1,9 +1,16 @@
+
 package recursion;
+import  java.lang.Math.*;
+
 
 public class Fibo {
     public static void main(String[] args) {
-        int ans = fibo(6);
-        System.out.println("Ans " + ans);
+//        int ans = fibo(6);
+//        System.out.println("Ans " + ans);
+        System.out.println(fiboFormula(40));
+    }
+    static int fiboFormula(int fibo){
+        return (int)(Math.pow(((1 + Math.sqrt(5)) / 2),fibo)/Math.sqrt(5));
     }
     static int fibo(int n ){
         if(n < 2 ){
