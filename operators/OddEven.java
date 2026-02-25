0c9f6061373cad77745409fcba1cd74781a8fad4
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-02-25 23:00:41 +0530

    Even or odd

---
+++ operators/OddEven.java
@@ -0,0 +1,13 @@
+package operators;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int n = 467;
+        System.out.println(isOdd(n));
+    }
+
+    private static boolean isOdd(int n) {
+        return ( n & 1) ==1;
+    }
+
+}
