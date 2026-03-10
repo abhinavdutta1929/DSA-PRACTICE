b128210c75ab79579f19e8bddfbb270dc902ac79
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-03-10 18:56:32 +0530

    Prime number

---
+++ operators/Sieve.java
@@ -0,0 +1,25 @@
+package operators;
+import java.util.*;
+
+public class Sieve {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        boolean[] arr = new boolean[n+1];
+        seive(n, arr);
+    }
+    static void seive(int n , boolean[] arr){
+        for(int i = 2 ; i * i <= n; i++){
+            if(arr[i] == false){
+                for(int j = i*2; j <= n ; j+=i){
+                    arr[j] = true;
+                }
+            }
+        }
+        for(int i = 2; i<=n ; i++){
+            if(!arr[i])
+            System.out.print(i + " ");
+        }
+    }
+}
