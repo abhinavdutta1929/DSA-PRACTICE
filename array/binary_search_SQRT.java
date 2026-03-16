4dbe9adb15db4d015aa905cda59777ef8909b5f4
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-03-17 01:05:37 +0530

    Implemented binary search method for square root

--- array/binary_search_SQRT.java
+++ array/binary_search_SQRT.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class binary_search_SQRT {
+    public static void main(String[] args) {
+        Scanner  sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int n = sc.nextInt();
+        System.out.println("Enter a decimal value");
+        int p = sc.nextInt();
+
+
+        System.out.println(sqrt(n , p));
+    }
+    static double sqrt(int n , int p){
+        int s = 0 ;
+        int e = n;
+         double root = 0.0;
+         while (s<=e){
+             int m = s +(e-s)/2;
+             if(m * m == n){
+                 return m;
+
+             } else if (m*m> n ) {
+                 e = m-1;
+             }
+             else {
+                 s = m+1;
+             }
+         }
+         double incr = 0.1;
+         for(int i = 0 ; i < p ; i++){
+             while(root * root <=n){
+                 root = root + incr;
+             }
+             root = root - incr;
+             incr/= 10;
+         }
+         return root;
+    }
 }
