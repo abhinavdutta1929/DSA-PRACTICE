1c3afc1e92a90eb8e08860c2301bf332cf0637fb
Abhinab Dutta <abhinavdutta123458@gmail.com>
2026-03-17 21:38:36 +0530

    printing 1 to n

---
+++ recursion/Function.java
@@ -0,0 +1,29 @@
+package recursion;
+
+public class Function {
+    public static void main(String[] args) {
+     funboth(6);
+    }
+    public static void fun(int n){
+        if(n == 0){
+            return;
+        }
+        System.out.println(n);
+        fun(n-1);
+    }
+    public static void funrev(int n){
+        if(n == 0){
+            return;
+        }
+        funrev(n-1);
+        System.out.println(n);
+    }
+    public static void funboth(int n ){
+        if(n == 0 ){
+            return ;
+        }
+        System.out.println(n);
+        funboth(n-1);
+        System.out.println(n);
+    }
+}
